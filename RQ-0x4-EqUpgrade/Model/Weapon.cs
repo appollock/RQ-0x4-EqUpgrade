3fa18382aa0ca86b3840a638c5c2376889987ff9
LazzR <41189421+appollock@users.noreply.github.com>
2021-03-22 14:19:49 +0100

    Implement Weapon model class

--- RQ-0x4-EqUpgrade/Model/Weapon.cs
+++ RQ-0x4-EqUpgrade/Model/Weapon.cs
@@ -2,7 +2,16 @@
 
 namespace RQ_0x4_EqUpgrade.Model
 {
-    public class Weapon
+    public class Weapon : IEquipment
     {
+        public string Name { get; set; }
+        public float MinimumAttackValue { get; set; }
+        public float MaximumAttackValue { get; set; }
+
+        public int Level { get; set; }
+        public float Strength { get; set; }
+        public float Agility { get; set; }
+        public float Wisdom { get; set; }
+        public float Health { get; set; }
     }
 }
