4a6dd04e880a6b5a10d363b979a82ba501803ed1
LazzR <41189421+appollock@users.noreply.github.com>
2021-03-22 14:21:05 +0100

    Implement Armor model class

--- RQ-0x4-EqUpgrade/Model/Armor.cs
+++ RQ-0x4-EqUpgrade/Model/Armor.cs
@@ -4,7 +4,15 @@ using System;
 
 namespace RQ_0x4_EqUpgrade.Model
 {
-    public class Armor
+    public class Armor : IEquipment
     {
+        public string Name { get; set; }
+        public float ArmorValue { get; set; }
+
+        public int Level { get; set; }
+        public float Strength { get; set; }
+        public float Agility { get; set; }
+        public float Wisdom { get; set; }
+        public float Health { get; set; }
     }
 }
