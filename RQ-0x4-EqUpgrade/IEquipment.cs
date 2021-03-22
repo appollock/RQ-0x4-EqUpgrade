a2731bd6b559c499a010852fe3e70fe214ba0f60
LazzR <41189421+appollock@users.noreply.github.com>
2021-03-22 14:15:08 +0100

    Add generic stats to IEquipment interface

--- RQ-0x4-EqUpgrade/IEquipment.cs
+++ RQ-0x4-EqUpgrade/IEquipment.cs
@@ -4,5 +4,10 @@ namespace RQ_0x4_EqUpgrade
 {
     public interface IEquipment
     {
+        public int Level { get; set; }
+        public float Strength { get; set; }
+        public float Agility { get; set; }
+        public float Wisdom { get; set; }
+        public float Health { get; set; }
     }
 }
