2e85f49662d66ff0fe81cc616b6bf85bd5345193
LazzR <41189421+appollock@users.noreply.github.com>
2021-03-22 14:15:44 +0100

    Implement upgrading generic stats in Blacksmith

--- RQ-0x4-EqUpgrade/Blacksmith.cs
+++ RQ-0x4-EqUpgrade/Blacksmith.cs
@@ -4,5 +4,39 @@ namespace RQ_0x4_EqUpgrade
 {
     public class Blacksmith
     {
+        const int MaxUpgradeLevel = 9;
+        const int ExpectedUpgradeTreshold = 2;
+        const float StatisticsMultiplier = 1.2F;
+
+        Random upgradeProbability;
+
+        public Blacksmith()
+        {
+            upgradeProbability = new Random();
+        }
+
+        public void Upgrade(ref IEquipment item)
+        {
+            if(item.Level < MaxUpgradeLevel && UpgradeSucceeded(item.Level))
+            {
+                item.Agility = UpgradeStat(item.Agility);
+                item.Health = UpgradeStat(item.Health);
+                item.Strength = UpgradeStat(item.Health);
+                item.Wisdom = UpgradeStat(item.Wisdom);
+                item.Level++;
+            }
+        }
+
+        private float UpgradeStat(float stat)
+        {
+            return stat * StatisticsMultiplier;
+        }
+
+        private bool UpgradeSucceeded(int equipmentLevel)
+        {
+            int result = upgradeProbability.Next(0, equipmentLevel);
+            int expected = equipmentLevel / ExpectedUpgradeTreshold;
+            return expected == result;
+        }
     }
 }
